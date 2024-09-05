0668fac7254ecead2bd6cf7c46401390ef9f6520
“Nasekin” <nasekindo@yandex.ru>
2024-09-05 11:29:59 +0500

    create app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
@@ -12,14 +12,14 @@ import Menu from '../Menu/Menu';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename='/app-tg'>
+      <HashRouter basename='/app-tg'>
         <Routes>
           <Route path="/" element={<WebSocketScreen/>}/>
           <Route path="weather" element={<WeatherScreen/>}/>
           <Route path="article" element={<InfiniteScrollComponent/>}/>
         </Routes>
         <Menu/>
-      </BrowserRouter>
+      </HashRouter>
       
     </div>
 
